f7f28e3b11b54c4ace628335a959a52a641f1baf
TimSpitzer95 <34818972+TimSpitzer95@users.noreply.github.com>
2021-10-31 16:42:23 +0100

    Update 2-user.sh

--- 2-user.sh
+++ 2-user.sh
@@ -25,29 +25,29 @@ ln -s "$HOME/zsh/.zshrc" $HOME/.zshrc
 PKGS=(
 'autojump'
 'awesome-terminal-fonts'
-'brave-bin' # Brave Browser
+#'brave-bin' # Brave Browser
 'dxvk-bin' # DXVK DirectX to Vulcan
-'github-desktop-bin' # Github Desktop sync
+#'github-desktop-bin' # Github Desktop sync
 'lightly-git'
-'mangohud' # Gaming FPS Counter
-'mangohud-common'
+#'mangohud' # Gaming FPS Counter
+#'mangohud-common'
 'nerd-fonts-fira-code'
-'nordic-darker-standard-buttons-theme'
-'nordic-darker-theme'
-'nordic-kde-git'
-'nordic-theme'
+#'nordic-darker-standard-buttons-theme'
+#'nordic-darker-theme'
+#'nordic-kde-git'
+#'nordic-theme'
 'noto-fonts-emoji'
-'papirus-icon-theme'
+#'papirus-icon-theme'
 'plasma-pa'
 'ocs-url' # install packages from websites
-'sddm-nordic-theme-git'
+#'sddm-nordic-theme-git'
 'snapper-gui-git'
 'ttf-droid'
 'ttf-hack'
 'ttf-meslo' # Nerdfont package
 'ttf-roboto'
-'zoom' # video conferences
-'snap-pac'
+#'zoom' # video conferences
+#'snap-pac'
 )
 
 for PKG in "${PKGS[@]}"; do
